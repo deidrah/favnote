75f8bcfefb44aa93973e9053199b080dea169937
Deidrah <dominikastasik93@gmail.com>
2021-05-30 19:29:21 +0200

    add header component

---
+++ src/views/NotesView/NotesView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotessView = () => (
+  <p>This is an Notes View</p>
+);
+
+export default NotessView;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./index.css";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ArticlesView from "../ArticlesView/ArticlesView";
+import NotesView from "../NotesView/NotesView";
+import TwittersView from "../TwittersView/TwittersView";
+import Header from "../../components/Header/Header";
 
 const initialStateItems = [
   {
@@ -34,9 +39,17 @@ class Root extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>hello world</h1>
-      </div>
+      <BrowserRouter>
+        <>
+          <Header />
+          <h1>Hello World</h1>
+          <Switch>
+            <Route exact path="/" component={TwittersView} />
+            <Route path="/articles" component={ArticlesView} />
+            <Route path="/notes" component={NotesView} />
+          </Switch>
+        </>
+      </BrowserRouter>
     );
   }
 }
